d9dcaedfc6aee2222697bc7f4c6e2ac6703a9b30
sapphi-red <green@sapphi.red>
2022-03-02 16:58:09 +0900

    chore: add lint

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module'
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [
+    {
+      // root files
+      files: ['*.{js,mjs}'],
+      excludedFiles: ['*/**/*.{js,mjs}'],
+      env: {
+        node: true
+      },
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off'
+      }
+    }
+  ],
+  ignorePatterns: ['dist', 'node_modules'],
+  reportUnusedDisableDirectives: true
+}
