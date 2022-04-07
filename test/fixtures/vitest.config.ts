0c23827ccacc05c5070b5b31fe2ba77838a38901
sapphi-red <green@sapphi.red>
2022-04-07 14:41:41 +0900

    test: disable trimRepositoryPrefix

--- test/fixtures/vitest.config.ts
+++ test/fixtures/vitest.config.ts
@@ -5,6 +5,8 @@ import GitHubActionsReporter from '../../src/index'
 
 export default defineConfig({
   test: {
-    reporters: new GitHubActionsReporter()
+    reporters: new GitHubActionsReporter({
+      trimRepositoryPrefix: false // make CI output is same with local
+    })
   }
 })
