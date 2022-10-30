fe7379384b4959779d3242842b27971279fdd225
sapphi-red <green@sapphi.red>
2022-10-30 16:28:54 +0900

    chore!: change node requirement to 14.16.0+
    vitest requires this

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  target: 'node14', // vitest supports vitest 14+
+  target: 'node14.16', // vitest supports vitest 14.16.0+
   format: ['cjs', 'esm'],
   dts: true,
   sourcemap: true,
